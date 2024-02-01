e6782aae50fc804f4dd9e542926ba607f4ddbd52
Lauren Beatty <lbeatty@edf.org>
2024-02-01 14:09:58 -0800

    place newgeneration at retired sites, bump up emissions factor of ccs based off of relative heat rates

--- run_airpollution.bat
+++ run_airpollution.bat
@@ -5,14 +5,32 @@ set evaldata=C:/Users/lbeatty/Documents/Lauren_MIP_Contribution/InMap/evaldata_v
 :: python MIP_AirPollution/Get_CAMD.py
 :: python MIP_AirPollution/Calculate_Model_Emissions.py
 
-set emissionsfile=InMap/MIP_Emissions/26z-thin-debug/SWITCH/emissions_2030.shp
+set "model=GenX"
+set "scenario=26z-thin-debug"
+
+set emissionsfile=InMap/MIP_Emissions/%scenario%/%model%/emissions_2030.shp
 set outputfile=emissions2030_out
+set outputloc=InMap/MIP_InMap_Output/%scenario%/%model%
 InMap\inmap-v1.9.6-windows-amd64.exe run steady --config=MIP_AirPollution\InMap_config.toml
 
-set emissionsfile=InMap/MIP_Emissions/26z-thin-debug/SWITCH/emissions_2040.shp
+set emissionsfile=InMap/MIP_Emissions/%scenario%/%model%/emissions_2040.shp
 set outputfile=emissions2040_out
+set outputloc=InMap/MIP_InMap_Output/%scenario%/%model%
 InMap\inmap-v1.9.6-windows-amd64.exe run steady --config=MIP_AirPollution\InMap_config.toml
 
-set emissionsfile=InMap/MIP_Emissions/26z-thin-debug/SWITCH/emissions_2050.shp
+set emissionsfile=InMap/MIP_Emissions/%scenario%/%model%/emissions_2050.shp
 set outputfile=emissions2050_out
+set outputloc=InMap/MIP_InMap_Output/%scenario%/%model%
 InMap\inmap-v1.9.6-windows-amd64.exe run steady --config=MIP_AirPollution\InMap_config.toml
+
+@REM set emissionsfile=InMap/MIP_Emissions/26z-thin-debug/SWITCH/emissions_2030.shp
+@REM set outputfile=emissions2030_out
+@REM InMap\inmap-v1.9.6-windows-amd64.exe run steady --config=MIP_AirPollution\InMap_config.toml
+
+@REM set emissionsfile=InMap/MIP_Emissions/26z-thin-debug/SWITCH/emissions_2040.shp
+@REM set outputfile=emissions2040_out
+@REM InMap\inmap-v1.9.6-windows-amd64.exe run steady --config=MIP_AirPollution\InMap_config.toml
+
+@REM set emissionsfile=InMap/MIP_Emissions/26z-thin-debug/SWITCH/emissions_2050.shp
+@REM set outputfile=emissions2050_out
+@REM InMap\inmap-v1.9.6-windows-amd64.exe run steady --config=MIP_AirPollution\InMap_config.toml
