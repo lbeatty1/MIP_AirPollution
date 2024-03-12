a55867fe787c6fc5976e4bd9f6738c38f913875f
Lauren Beatty <lbeatty@edf.org>
2024-03-12 16:43:48 -0700

    jobs stuff add

---
+++ jobs.py
@@ -0,0 +1,12 @@
+globals().clear()
+
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import pandas as pd
+import math
+
+### define model/scenario
+model = 'GenX'
+scenario = '26z-short-base-50'
