8a777baf455c4b430545727cc523dfd76ee149b1
Lauren Beatty <lbeatty@edf.org>
2024-06-20 15:33:08 -0700

    big restructure of repo

---
+++ Downscaled/settings.py
@@ -0,0 +1,17 @@
+year_inputs = {
+    'p1': 2027,
+    'p2': 2030,
+    'p3': 2035,
+    'p4': 2040,
+    'p5': 2045,
+    'p6': 2050
+}
+print("year_inputs loaded")
+
+
+eia_datapath = 'Data/'
+epa_datapath = 'Data/'
+print("path settings loaded")
+
+ccs_heatrate_penalty =  6.36/7.16
+print("ccspenalty settings loaded")
